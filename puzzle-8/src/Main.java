4d46811b4e6f24befacf1f9470842c96cc5367f9
GerardoPz <pliegolugohectorgerardo@gmail.com>
2024-10-08 17:43:24 -0600

    Movimiento de piezas y verificar si ganaste

--- puzzle-8/src/Main.java
+++ puzzle-8/src/Main.java
@@ -1,3 +1,5 @@
+import puzzle.ExcepcionDePilaLlena;
+import puzzle.ExcepcionDePilaVacia;
 import puzzle.PilaEstatica;
 import puzzle.Puzzle8;
 
@@ -7,7 +9,14 @@ public class Main {
     public static void main(String[] args) {
         PilaEstatica<Integer> pilaEstatica = new PilaEstatica<>(1);
         SwingUtilities.invokeLater(() -> {
-            Puzzle8 puzzle = new Puzzle8();
+            Puzzle8 puzzle = null;
+            try {
+                puzzle = new Puzzle8();
+            } catch (ExcepcionDePilaLlena e) {
+                throw new RuntimeException(e);
+            } catch (ExcepcionDePilaVacia e) {
+                throw new RuntimeException(e);
+            }
             puzzle.setVisible(true);
         });
     }
